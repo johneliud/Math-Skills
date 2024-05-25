71e41ed3522484fb215a4b74c71ff775d26c6103
JohnEliud <johneliud4@gmail.com>
2024-05-25 13:52:05 +0300

    Chore: 1. Defined FindAverage, FindMedian, Find Variance and FindStandardDeviation functions. 2. Defined ReadAndProcess function which uses a bufio scanner to read the input file contents and pass it through various functions.

--- calculations/calculations.go
+++ calculations/calculations.go
@@ -1,14 +1,48 @@
 package calculations
 
+import "math"
+
 func FindAverage(num []float64) float64 {
-	return 0.0
+	var sum, average float64
+
+	sliceLength := len(num)
+
+	for i := 0; i < sliceLength; i++ {
+		sum += num[i]
+	}
+	average = sum / float64(sliceLength)
+
+	return average
 }
 func FindMedian(num []float64) float64 {
-	return 0.0
+	var sum, median float64
+
+	sliceLength := len(num)
+
+	if sliceLength%2 == 1 {
+		median = num[sliceLength/2]
+	} else if sliceLength%2 == 0 {
+		sum = num[(sliceLength/2)] + num[(sliceLength/2)-1]
+		median = sum / 2
+	}
+
+	return median
 }
 func FindVariance(num []float64) float64 {
-	return 0.0
+	var sum, variance float64
+
+	meanOfSlice := FindAverage(num)
+	sliceLength := len(num)
+
+	for i := 0; i < sliceLength; i++ {
+		sum = (num[i] - meanOfSlice) * (num[i] - meanOfSlice)
+	}
+	variance += sum
+
+	return variance
 }
 func FindStandardDeviation(num float64) float64 {
-	return 0.0
+	standardDeviation := math.Sqrt(num)
+
+	return standardDeviation
 }
--- readandprocess/readandprocess.go
+++ readandprocess/readandprocess.go
@@ -1,5 +1,104 @@
 package readandprocess
 
+import (
+	"Math-Skills/calculations"
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
 func ReadAndProcess(str string) string {
-	return ""
+	dataTxt, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println("Error opening input file!", err)
+		os.Exit(1)
+	}
+	defer dataTxt.Close()
+
+	dataTxtDetails, err := dataTxt.Stat()
+	if err != nil {
+		fmt.Println("Error getting file details!", err)
+		os.Exit(1)
+	}
+
+	if dataTxtDetails.Size() == 0 {
+		fmt.Println("Error. Input file is empty!")
+		os.Exit(1)
+	}
+
+	scanner := bufio.NewScanner(dataTxt)
+
+	var valueSlice []float64
+
+	for scanner.Scan() {
+		readLine := scanner.Text()
+
+		if readLine == "" {
+			continue
+		}
+
+		floatValue, err := strconv.ParseFloat(readLine, 64)
+		if err != nil {
+			fmt.Println("Error converting to float64!", err)
+			os.Exit(1)
+		}
+
+		if floatValue >= math.MaxInt64 {
+			fmt.Println("Error. Float value is out of range!")
+			os.Exit(1)
+		}
+
+		if floatValue < 0 {
+			fmt.Println("Error. Float value should not be a negative value!")
+			os.Exit(1)
+		}
+		valueSlice = append(valueSlice, floatValue)
+	}
+
+	if len(valueSlice) == 1 {
+		fmt.Println("Error. Statistics cannot be calculated from a single value!")
+		os.Exit(1)
+	}
+
+	average := calculations.FindAverage(valueSlice)
+	median := calculations.FindMedian(valueSlice)
+	variance := calculations.FindVariance(valueSlice)
+	standardDeviation := calculations.FindStandardDeviation(variance)
+
+	// Round results of the calculations
+	roundedAverage := math.Round(average)
+	roundedMedian := math.Round(median)
+	roundedVariance := math.Round(variance)
+	roundedStandardDeviation := math.Round(standardDeviation)
+
+	var statisticsResult string
+
+	// Check if rounded calculations are greater than allowed range
+	if roundedAverage >= math.MaxInt64 {
+		statisticsResult += "Error. Average value out of range!\n"
+	} else {
+		statisticsResult += fmt.Sprintf("Average: %.0f\n", roundedAverage)
+	}
+
+	if roundedMedian >= math.MaxInt64 {
+		statisticsResult += "Error. Median value out of range!\n"
+	} else {
+		statisticsResult += fmt.Sprintf("Median: %.0f\n", roundedMedian)
+	}
+
+	if roundedVariance >= math.MaxInt64 {
+		statisticsResult += "Error. Variance value out of range!\n"
+	} else {
+		statisticsResult += fmt.Sprintf("Variance: %.0f\n", roundedVariance)
+	}
+
+	if roundedStandardDeviation >= math.MaxInt64 {
+		statisticsResult += "Error. Standard Deviation value out of range!\n"
+	} else {
+		statisticsResult += fmt.Sprintf("Standard Deviation: %.0f\n", roundedStandardDeviation)
+	}
+
+	return statisticsResult
 }
