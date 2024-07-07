a8a2dd179c17e524719b3688db9cd7d0a4df8f7b
John <johneliud4@gmail.com>
2024-07-07 17:06:49 +0300

    Chore: Fix function FindStandardDeviation() to take a slice of float64 values. The function then returns the squareroot of the FindVariance() function

--- calculations/calculations.go
+++ calculations/calculations.go
@@ -2,50 +2,50 @@ package calculations
 
 import "math"
 
-func FindAverage(num []float64) float64 {
+func FindAverage(nums []float64) float64 {
 	var sum, average float64
 
-	sliceLength := len(num)
+	sliceLength := len(nums)
 
 	for i := 0; i < sliceLength; i++ {
-		sum += num[i]
+		sum += nums[i]
 	}
 	average = sum / float64(sliceLength)
 
 	return average
 }
 
-func FindMedian(num []float64) float64 {
+func FindMedian(nums []float64) float64 {
 	var sum, median float64
 
-	sliceLength := len(num)
+	sliceLength := len(nums)
 
 	if sliceLength%2 == 1 {
-		median = num[sliceLength/2]
+		median = nums[sliceLength/2]
 	} else if sliceLength%2 == 0 {
-		sum = num[(sliceLength/2)] + num[(sliceLength/2)-1]
+		sum = nums[(sliceLength/2)] + nums[(sliceLength/2)-1]
 		median = sum / 2
 	}
 
 	return median
 }
 
-func FindVariance(num []float64) float64 {
+func FindVariance(nums []float64) float64 {
 	var sum, variance float64
 
-	meanOfSlice := FindAverage(num)
-	sliceLength := len(num)
+	meanOfSlice := FindAverage(nums)
+	sliceLength := len(nums)
 
 	for i := 0; i < sliceLength; i++ {
-		sum += (num[i] - meanOfSlice) * (num[i] - meanOfSlice)
+		sum += (nums[i] - meanOfSlice) * (nums[i] - meanOfSlice)
 	}
 	variance = sum / float64(sliceLength)
 
 	return variance
 }
 
-func FindStandardDeviation(num float64) float64 {
-	standardDeviation := math.Sqrt(num)
+func FindStandardDeviation(nums []float64) float64 {
+	standardDeviation := math.Sqrt(FindVariance(nums))
 
 	return standardDeviation
 }
